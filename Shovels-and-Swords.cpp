b35d4081e28a624b8d7810df3e9dc7a0b1b25d26
CODEWITH-SB <bhowmicksusmoy067@gmail.com>
2026-03-26 12:59:13 +0600

    https://codeforces.com/problemset/problem/1366/A

---
+++ Shovels-and-Swords.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEC2BIN(x, bits) (bitset<bits>(x).to_string())
+#define vi vector<int>
+#define pb push_back
+#define pob pop_back
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define test cout << "THIS POSITION IS OK" << endl
+#define sp cout << " "
+#define eps 1e-10
+#define sz(x) x.size()
+#define gcd __gcd
+#define vec2d(ve, r, c) vector<vector<int>> ve(r, vector<int>(c, 0))
+#define dsort(x) sort(x.begin(), x.end(), greater<int>())
+#define maxx INT_MAX
+#define inttos(x) std::to_string(x)
+#define stoint(s) std::stoi(s)
+
+void solve()
+{
+    ll x, y, c = 0;
+    cin >> x >> y;
+    c = min({x, y, (x + y) / 3});
+    cout << c;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    return 0;
+}
