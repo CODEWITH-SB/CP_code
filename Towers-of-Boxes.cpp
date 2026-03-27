1fadf76b31c8f28dd82c41b424c33a66fe4bbca2
CODEWITH-SB <bhowmicksusmoy067@gmail.com>
2026-03-27 13:39:18 +0600

    https://codeforces.com/contest/2203/problem/A

---
+++ Towers-of-Boxes.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEC2BIN(x, bits) (bitset<bits>(x).to_string())
+#define vi vector<int>
+#define pb push_back
+#define pob pop_back
+#define ll long long int
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define test cout << "THIS POSITION IS OK" << endl
+#define sp cout << " "
+#define eps 1e-10
+#define sz(x) x.size()
+#define gcd __gcd
+#define vec2d(ve, r, c) vector<vector<int>> ve(r, vector<int>(c, 0))
+#define dsort(x) sort(x.begin(), x.end(), greater<int>())
+#define maxx INT_MAX
+#define inttos(x) std::to_string(x)
+#define stoint(s) std::stoi(s)
+
+void solve()
+{
+
+    int n, k, d;
+    cin >> n >> k >> d;
+    int ans = ceil((double)n / ((d / k) + 1));
+    if (k > d)
+        cout << n;
+    else
+        cout << ans;
+};
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << "\n";
+    }
+
+    return 0;
+}
